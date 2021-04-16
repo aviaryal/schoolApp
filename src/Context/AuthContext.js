6b1bac7aa4728da17574ec350c98bfa30b790390
aviaryal <58341812+aviaryal@users.noreply.github.com>
2021-04-16 18:35:30 -0500

    first

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,14 +1,28 @@
+import React from 'react';
+import schoolapi from '../api/schoolapi';
+import CreateContext from './CreateContext';
 
 const authReducer = (state,action) =>{
 
 }
 
-const signin = (dispatch) => ({email,password}) =>{
+const signin = (dispatch) => async ({email,password}) =>{
     try
     {
-
+        const response = await schoolapi.post('/signin',{email,password});
+        
     }
     catch(err){
 
     }
 };
+
+const signout = (dispatch) => async()=>{
+
+}
+
+export const {Provider, Context} = CreateContext(
+    authReducer,
+    {signin, signout,signup},
+    {}
+);
