ad0c15d1457b95c3c0c850409b8ce0266c6c7feb
sbhandari0817 <santosh.bhandari2@mavs.uta.edu>
2021-08-05 19:10:40 -0500

    updated signin and profile UI'
    q
    h

--- src/Screens/SignInScreen.js
+++ src/Screens/SignInScreen.js
@@ -1,78 +1,103 @@
-import React from 'react';
-
-import { useState, useContext } from 'react';
-import {View,Text,StyleSheet, TextInput, Button, Picker, TouchableOpacity} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 import {AuthContext} from '../Context/AuthContext.js';
 const SignInScreen= ()=>{
     const {auth} = React.useContext(AuthContext);
     const {signIn} = auth;
     const [email,setEmail] = useState('');
     const [password, setPassword] = useState('');
-    
-    return (
-        <View style={styles.container}>
-            <View style={styles.inputView}>
-                <TextInput
-                    value={email}
-                    placeholder="Email"
-                    autoCapitalize='none'
-                    autoCorrect={false}
-                    onChangeText={setEmail}
-                    style= {styles.textInput}
-                />
-            </View>
-            <View style={styles.inputView}>
-                <TextInput
-                    style= {styles.textInput}
-                    value={password}
-                    placeholder="Password"
-                    autoCapitalize='none'
-                    autoCorrect={false}
-                    onChangeText={setPassword}
-                    secureTextEntry={true}
-                />
-            </View>
-            <TouchableOpacity style={styles.button}
-                onPress={()=>signIn({email,password})}>
-                <Text> Sign in</Text>
-            </TouchableOpacity>
-            
 
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={require('./logo.png')} />
+
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email."
+          placeholderTextColor="#ffffff"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password."
+          placeholderTextColor="#FFFFFF"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.loginBtn} onPress={()=>signIn({email,password})}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        // paddingHorizontal: 20,
-        // paddingTop: 20,
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        backgroundColor:'#0ff',
-    },
-    inputView:{
-        width:"80%",
-        backgroundColor:"#fff",
-        borderRadius:25,
-        height:50,
-        marginBottom:20,
-        alignItems:'center',
-        
-    },
-    textInput: {
-        borderWidth:1,
-        flex:1,
-        height:50,
-        color: '#05375a',
-        borderColor:'#fff'
-    },
-    button: {
-        alignItems: "center",
-        backgroundColor: "#DDDDDD",
-        padding: 10
-      },
-    
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  image: {
+    marginBottom: 20,
+    width: 300,
+    height: 200
+  },
+
+  inputView: {
+    backgroundColor: "#fa5b3d",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+
+    alignItems: "center",
+  },
+
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+    color:'white'
+  },
+
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#fa5b3d",
+  },
 });
 
 export default SignInScreen;
