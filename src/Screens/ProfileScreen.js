f1aa82b54f5c5b7aa78d97c7e8f7a0ff10809371
sbhandari0817 <santosh.bhandari2@mavs.uta.edu>
2021-08-05 19:35:44 -0500

    added logo

--- src/Screens/ProfileScreen.js
+++ src/Screens/ProfileScreen.js
@@ -64,17 +64,6 @@ const styles = StyleSheet.create({
         width: Dimensions.get('window').width,
         height: 200
       },
-    avatar: {
-        width: 130,
-        height: 130,
-        borderRadius: 63,
-        borderWidth: 4,
-        borderColor: "white",
-        marginBottom: 10,
-        alignSelf: 'center',
-        position: 'absolute',
-        marginTop: 130
-    },
     screen: {
         fontSize: 28,
         fontWeight: 'bold',
